ed51e499d65547e64721744e8176abbac7135170
Yannick Kirchhoff <yannick.kirchhoff@dkfz-heidelberg.de>
2024-03-22 12:00:56 +0100

    bounding box dataloader and trainer

--- nnunetv2/training/dataloading/data_loader_2d_bbox.py
+++ nnunetv2/training/dataloading/data_loader_2d_bbox.py
@@ -19,44 +19,51 @@ class nnUNetDataLoader2DBBOX(nnUNetDataLoaderBase):
         self.bbox_dilation = bbox_dilation
 
     def get_bbox(self, seg: np.ndarray):
-        uniques = np.unique(seg)[1:]
-        cls = np.random.choice(uniques)
-        if self.footprint is not None:
-            struct = np.ones((self.footprint,)*2, dtype=np.uint8)
-            dilated_seg = dilation((seg[0]==cls).astype(np.uint8), struct)
-        else:
-            dilated_seg = (seg[0] == cls).astype(np.uint8)
-        seg_inst, seg_num = label(dilated_seg, return_num=True)
-        if seg_num > 1:
-            rand_inst = np.random.randint(1, seg_num+1)
-            seg_inst = np.where(seg_inst == rand_inst, 1, 0).astype(np.uint8)
-        seg_inst[seg[0]==0] = 0
-        w = np.where(seg_inst == 1)
-        bbox = [np.min(w[0]), np.max(w[0])+1, np.min(w[1]), np.max(w[1])+1]
-        size = [bbox[1]-bbox[0], bbox[3]-bbox[2]]
-        need_to_pad = self.need_to_pad.copy()
-        dim = seg.ndim-1
-        shape = seg.shape[1:]
-        for d in range(dim):
-            # if case_all_data.shape + need_to_pad is still < patch size we need to pad more! We pad on both sides
-            # always
-            if need_to_pad[d] + shape[d] < self.patch_size[d]:
-                need_to_pad[d] = self.patch_size[d] - shape[d]
-        bbox_lbs = [np.random.randint(bbox[0]-self.patch_size[0]+size[0], bbox[0]+1),
-                    np.random.randint(bbox[2]-self.patch_size[1]+size[1], bbox[2]+1)]
-        bbox_ubs = [bbox_lbs[i] + self.patch_size[i] for i in range(dim)]
-        bbox_mask = np.zeros_like(seg[0])
-        jitter = [np.random.randint(0, self.bbox_dilation+1, size=4)]
-        bbox_mask[bbox[0]-jitter[0]:bbox[1]+jitter[1], bbox[2]-jitter[2]:bbox[3]+jitter[3]] = 1
+        uniques = np.unique(seg)
+        if np.any(uniques>0):
+            cls = np.random.choice(uniques[uniques>0])
+            if self.footprint is not None:
+                struct = np.ones((self.footprint,)*2, dtype=np.uint8)
+                dilated_seg = dilation((seg[0]==cls).astype(np.uint8), struct)
+            else:
+                dilated_seg = (seg[0] == cls).astype(np.uint8)
+            seg_inst, seg_num = label(dilated_seg, return_num=True)
+            if seg_num > 1:
+                rand_inst = np.random.randint(1, seg_num+1)
+                seg_inst = np.where(seg_inst == rand_inst, 1, 0).astype(np.uint8)
+            seg_inst[seg[0]==0] = 0
+            w = np.where(seg_inst == 1)
+            bbox = [np.min(w[0]), np.max(w[0])+1, np.min(w[1]), np.max(w[1])+1]
+            size = [bbox[1]-bbox[0], bbox[3]-bbox[2]]
+            bbox_lbs = [np.random.randint(bbox[0]-max(0, self.patch_size[0]-size[0]), bbox[0]+1),
+                        np.random.randint(bbox[2]-max(0, self.patch_size[1]-size[1]), bbox[2]+1)]
+            bbox_ubs = [bbox_lbs[i] + self.patch_size[i] for i in range(2)]
+            bbox_mask = np.zeros_like(seg)
+            jitter = np.random.randint(0, self.bbox_dilation+1, size=4)
+            bbox_mask[:, bbox[0]-jitter[0]:bbox[1]+jitter[1], bbox[2]-jitter[2]:bbox[3]+jitter[3]] = 1
 
-        return bbox_lbs, bbox_ubs, seg_inst[None].astype(seg.dtype), bbox_mask[None].astype(seg.dtype)
+            return bbox_lbs, bbox_ubs, np.concatenate([seg_inst[None].astype(seg.dtype), bbox_mask.astype(seg.dtype)], axis=0)
+        else:
+            need_to_pad = self.need_to_pad.copy()
+            shape = seg.shape[1:]
+
+            for d in range(2):
+                if need_to_pad[d] + shape[d] < self.patch_size[d]:
+                    need_to_pad[d] = self.patch_size[d] - shape[d]
+
+            lbs = [- need_to_pad[i] // 2 for i in range(2)]
+            ubs = [shape[i] + need_to_pad[i] // 2 + need_to_pad[i] % 2 - self.patch_size[i] for i in range(2)]
+
+            bbox_lbs = [np.random.randint(lbs[i], ubs[i] + 1) for i in range(2)]
+            bbox_ubs = [bbox_lbs[i] + self.patch_size[i] for i in range(2)]
+
+            return bbox_lbs, bbox_ubs, np.concatenate([seg, np.zeros_like(seg)], axis=0)
 
     def generate_train_batch(self):
         selected_keys = self.get_indices()
         # preallocate memory for data and seg
         data_all = np.zeros(self.data_shape, dtype=np.float32)
-        seg_all = np.zeros(self.seg_shape, dtype=np.int16)
-        bbox_mask_all = np.zeros(self.seg_shape, dtype=np.int16)
+        seg_all = np.zeros((self.seg_shape[0], 2, *self.seg_shape[2:]), dtype=np.int16)
         case_properties = []
 
         for j, current_key in enumerate(selected_keys):
@@ -73,7 +80,7 @@ class nnUNetDataLoader2DBBOX(nnUNetDataLoaderBase):
             # print(properties)
             shape = data.shape[1:]
             dim = len(shape)
-            bbox_lbs, bbox_ubs, seg, bbox_mask = self.get_bbox(seg)
+            bbox_lbs, bbox_ubs, seg = self.get_bbox(seg)
 
             # whoever wrote this knew what he was doing (hint: it was me). We first crop the data to the region of the
             # bbox that actually lies within the data. This will result in a smaller array which is then faster to pad.
@@ -91,14 +98,12 @@ class nnUNetDataLoader2DBBOX(nnUNetDataLoaderBase):
 
             this_slice = tuple([slice(0, seg.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
             seg = seg[this_slice]
-            bbox_mask = bbox_mask[this_slice]
 
             padding = [(-min(0, bbox_lbs[i]), max(bbox_ubs[i] - shape[i], 0)) for i in range(dim)]
             data_all[j] = np.pad(data, ((0, 0), *padding), 'constant', constant_values=0)
             seg_all[j] = np.pad(seg, ((0, 0), *padding), 'constant', constant_values=-1)
-            bbox_mask_all[j] = np.pad(bbox_mask, ((0, 0), *padding), 'constant', constant_values=0)
 
-        return {'data': data_all, 'seg': seg_all, 'bbox_mask': bbox_mask_all, 'properties': case_properties, 'keys': selected_keys}
+        return {'data': data_all, 'seg': seg_all, 'properties': case_properties, 'keys': selected_keys}
 
 
 if __name__ == '__main__':
---
+++ nnunetv2/training/nnUNetTrainer/variants/coding_power_unit/nnUNetTrainerCPU.py
@@ -0,0 +1,335 @@
+import inspect
+import multiprocessing
+import os
+import shutil
+import sys
+import warnings
+from copy import deepcopy
+from datetime import datetime
+from time import time, sleep
+from typing import Union, Tuple, List
+
+import numpy as np
+import torch
+from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
+from batchgenerators.dataloading.nondet_multi_threaded_augmenter import NonDetMultiThreadedAugmenter
+from batchgenerators.dataloading.single_threaded_augmenter import SingleThreadedAugmenter
+from batchgenerators.transforms.abstract_transforms import AbstractTransform, Compose
+from batchgenerators.transforms.color_transforms import BrightnessMultiplicativeTransform, \
+    ContrastAugmentationTransform, GammaTransform
+from batchgenerators.transforms.noise_transforms import GaussianNoiseTransform, GaussianBlurTransform
+from batchgenerators.transforms.resample_transforms import SimulateLowResolutionTransform
+from batchgenerators.transforms.spatial_transforms import SpatialTransform, MirrorTransform
+from batchgenerators.transforms.utility_transforms import RemoveLabelTransform, RenameTransform, NumpyToTensor
+from batchgenerators.utilities.file_and_folder_operations import join, load_json, isfile, save_json, maybe_mkdir_p
+from torch._dynamo import OptimizedModule
+
+from nnunetv2.configuration import ANISO_THRESHOLD, default_num_processes
+from nnunetv2.evaluation.evaluate_predictions import compute_metrics_on_folder
+from nnunetv2.inference.export_prediction import export_prediction_from_logits, resample_and_save
+from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from nnunetv2.inference.sliding_window_prediction import compute_gaussian
+from nnunetv2.paths import nnUNet_preprocessed, nnUNet_results
+from nnunetv2.training.data_augmentation.compute_initial_patch_size import get_patch_size
+from nnunetv2.training.data_augmentation.custom_transforms.cascade_transforms import MoveSegAsOneHotToData, \
+    ApplyRandomBinaryOperatorTransform, RemoveRandomConnectedComponentFromOneHotEncodingTransform
+from nnunetv2.training.data_augmentation.custom_transforms.deep_supervision_donwsampling import \
+    DownsampleSegForDSTransform2
+from nnunetv2.training.data_augmentation.custom_transforms.limited_length_multithreaded_augmenter import \
+    LimitedLenWrapper
+from nnunetv2.training.data_augmentation.custom_transforms.masking import MaskTransform
+from nnunetv2.training.data_augmentation.custom_transforms.region_based_training import \
+    ConvertSegmentationToRegionsTransform
+from nnunetv2.training.data_augmentation.custom_transforms.transforms_for_dummy_2d import Convert2DTo3DTransform, \
+    Convert3DTo2DTransform
+from nnunetv2.training.dataloading.data_loader_2d_bbox import nnUNetDataLoader2DBBOX
+from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDataset
+from nnunetv2.training.dataloading.utils import get_case_identifiers, unpack_dataset
+from nnunetv2.training.logging.nnunet_logger import nnUNetLogger
+from nnunetv2.training.loss.compound_losses import DC_and_CE_loss, DC_and_BCE_loss
+from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
+from nnunetv2.training.loss.dice import get_tp_fp_fn_tn, MemoryEfficientSoftDiceLoss
+from nnunetv2.training.lr_scheduler.polylr import PolyLRScheduler
+from nnunetv2.utilities.collate_outputs import collate_outputs
+from nnunetv2.utilities.crossval_split import generate_crossval_split
+from nnunetv2.utilities.default_n_proc_DA import get_allowed_n_proc_DA
+from nnunetv2.utilities.file_path_utilities import check_workers_alive_and_busy
+from nnunetv2.utilities.get_network_from_plans import get_network_from_plans
+from nnunetv2.utilities.helpers import empty_cache, dummy_context
+from nnunetv2.utilities.label_handling.label_handling import convert_labelmap_to_one_hot, determine_num_input_channels
+from nnunetv2.utilities.plans_handling.plans_handler import PlansManager, ConfigurationManager
+from torch import autocast, nn
+from torch import distributed as dist
+from torch.cuda import device_count
+from torch.cuda.amp import GradScaler
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainerCPU(nnUNetTrainer):
+    @staticmethod
+    def build_network_architecture(architecture_class_name: str,
+                                   arch_init_kwargs: dict,
+                                   arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
+                                   num_input_channels: int,
+                                   num_output_channels: int,
+                                   enable_deep_supervision: bool = True) -> nn.Module:
+        return get_network_from_plans(
+            architecture_class_name,
+            arch_init_kwargs,
+            arch_init_kwargs_req_import,
+            num_input_channels+1,
+            num_output_channels,
+            allow_init=True,
+            deep_supervision=enable_deep_supervision)
+
+    @staticmethod
+    def get_training_transforms(
+            patch_size: Union[np.ndarray, Tuple[int]],
+            rotation_for_DA: dict,
+            deep_supervision_scales: Union[List, Tuple, None],
+            mirror_axes: Tuple[int, ...],
+            do_dummy_2d_data_aug: bool,
+            order_resampling_data: int = 3,
+            order_resampling_seg: int = 1,
+            border_val_seg: int = -1,
+            use_mask_for_norm: List[bool] = None,
+            is_cascaded: bool = False,
+            foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+            regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+            ignore_label: int = None,
+    ) -> AbstractTransform:
+        tr_transforms = []
+        if do_dummy_2d_data_aug:
+            ignore_axes = (0,)
+            tr_transforms.append(Convert3DTo2DTransform())
+            patch_size_spatial = patch_size[1:]
+        else:
+            patch_size_spatial = patch_size
+            ignore_axes = None
+
+        tr_transforms.append(SpatialTransform(
+            patch_size_spatial, patch_center_dist_from_border=None,
+            do_elastic_deform=False, alpha=(0, 0), sigma=(0, 0),
+            do_rotation=True, angle_x=rotation_for_DA['x'], angle_y=rotation_for_DA['y'], angle_z=rotation_for_DA['z'],
+            p_rot_per_axis=1,  # todo experiment with this
+            do_scale=True, scale=(0.7, 1.4),
+            border_mode_data="constant", border_cval_data=0, order_data=order_resampling_data,
+            border_mode_seg="constant", border_cval_seg=border_val_seg, order_seg=order_resampling_seg,
+            random_crop=False,  # random cropping is part of our dataloaders
+            p_el_per_sample=0, p_scale_per_sample=0.2, p_rot_per_sample=0.2,
+            independent_scale_for_each_axis=False  # todo experiment with this
+        ))
+
+        if do_dummy_2d_data_aug:
+            tr_transforms.append(Convert2DTo3DTransform())
+
+        tr_transforms.append(GaussianNoiseTransform(p_per_sample=0.1))
+        tr_transforms.append(GaussianBlurTransform((0.5, 1.), different_sigma_per_channel=True, p_per_sample=0.2,
+                                                   p_per_channel=0.5))
+        tr_transforms.append(BrightnessMultiplicativeTransform(multiplier_range=(0.75, 1.25), p_per_sample=0.15))
+        tr_transforms.append(ContrastAugmentationTransform(p_per_sample=0.15))
+        tr_transforms.append(SimulateLowResolutionTransform(zoom_range=(0.5, 1), per_channel=True,
+                                                            p_per_channel=0.5,
+                                                            order_downsample=0, order_upsample=3, p_per_sample=0.25,
+                                                            ignore_axes=ignore_axes))
+        tr_transforms.append(GammaTransform((0.7, 1.5), True, True, retain_stats=True, p_per_sample=0.1))
+        tr_transforms.append(GammaTransform((0.7, 1.5), False, True, retain_stats=True, p_per_sample=0.3))
+
+        if mirror_axes is not None and len(mirror_axes) > 0:
+            tr_transforms.append(MirrorTransform(mirror_axes))
+
+        if use_mask_for_norm is not None and any(use_mask_for_norm):
+            tr_transforms.append(MaskTransform([i for i in range(len(use_mask_for_norm)) if use_mask_for_norm[i]],
+                                               mask_idx_in_seg=0, set_outside_to=0))
+
+        tr_transforms.append(RemoveLabelTransform(-1, 0))
+
+        if is_cascaded:
+            assert foreground_labels is not None, 'We need foreground_labels for cascade augmentations'
+            tr_transforms.append(MoveSegAsOneHotToData(1, foreground_labels, 'seg', 'data'))
+            tr_transforms.append(ApplyRandomBinaryOperatorTransform(
+                channel_idx=list(range(-len(foreground_labels), 0)),
+                p_per_sample=0.4,
+                key="data",
+                strel_size=(1, 8),
+                p_per_label=1))
+            tr_transforms.append(
+                RemoveRandomConnectedComponentFromOneHotEncodingTransform(
+                    channel_idx=list(range(-len(foreground_labels), 0)),
+                    key="data",
+                    p_per_sample=0.2,
+                    fill_with_other_class_p=0,
+                    dont_do_if_covers_more_than_x_percent=0.15))
+
+        tr_transforms.append(RenameTransform('seg', 'target', True))
+
+        if regions is not None:
+            # the ignore label must also be converted
+            tr_transforms.append(ConvertSegmentationToRegionsTransform(list(regions) + [ignore_label]
+                                                                       if ignore_label is not None else regions,
+                                                                       'target', 'target'))
+
+        if deep_supervision_scales is not None:
+            tr_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, input_key='target',
+                                                              output_key='target'))
+        tr_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
+        tr_transforms = Compose(tr_transforms)
+        return tr_transforms
+
+    @staticmethod
+    def get_validation_transforms(
+            deep_supervision_scales: Union[List, Tuple, None],
+            is_cascaded: bool = False,
+            foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+            regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+            ignore_label: int = None,
+    ) -> AbstractTransform:
+        val_transforms = []
+        val_transforms.append(RemoveLabelTransform(-1, 0))
+
+        if is_cascaded:
+            val_transforms.append(MoveSegAsOneHotToData(1, foreground_labels, 'seg', 'data'))
+
+        val_transforms.append(RenameTransform('seg', 'target', True))
+
+        if regions is not None:
+            # the ignore label must also be converted
+            val_transforms.append(ConvertSegmentationToRegionsTransform(list(regions) + [ignore_label]
+                                                                        if ignore_label is not None else regions,
+                                                                        'target', 'target'))
+
+        if deep_supervision_scales is not None:
+            val_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, input_key='target',
+                                                               output_key='target'))
+
+        val_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
+        val_transforms = Compose(val_transforms)
+        return val_transforms
+
+    def get_plain_dataloaders(self, initial_patch_size: Tuple[int, ...], dim: int):
+        dataset_tr, dataset_val = self.get_tr_and_val_datasets()
+
+        if dim == 2:
+            dl_tr = nnUNetDataLoader2DBBOX(dataset_tr, self.batch_size,
+                                       initial_patch_size,
+                                       self.configuration_manager.patch_size,
+                                       self.label_manager,
+                                       oversample_foreground_percent=self.oversample_foreground_percent,
+                                       sampling_probabilities=None, pad_sides=None,
+                                       footprint=5, bbox_dilation=20)
+            dl_val = nnUNetDataLoader2DBBOX(dataset_val, self.batch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.label_manager,
+                                        oversample_foreground_percent=self.oversample_foreground_percent,
+                                        sampling_probabilities=None, pad_sides=None,
+                                        footprint=5, bbox_dilation=20)
+        else:
+            raise NotImplementedError("3D dataloader not implemented")
+        return dl_tr, dl_val
+
+    def train_step(self, batch: dict) -> dict:
+        data = batch['data']
+        target = batch['target']
+
+        data = data.to(self.device, non_blocking=True)
+        if isinstance(target, list):
+            bbox_mask = target[0][:,-1:].to(self.device, non_blocking=True)
+            target = [i[:,:-1].to(self.device, non_blocking=True) for i in target]
+        else:
+            bbox_mask = target[:,-1:].to(self.device, non_blocking=True)
+            target = target[:,:-1].to(self.device, non_blocking=True)
+
+        data = torch.cat([data, bbox_mask], dim=1)
+
+        self.optimizer.zero_grad(set_to_none=True)
+        # Autocast can be annoying
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+            output = self.network(data)
+            # del data
+            l = self.loss(output, target)
+
+        if self.grad_scaler is not None:
+            self.grad_scaler.scale(l).backward()
+            self.grad_scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.grad_scaler.step(self.optimizer)
+            self.grad_scaler.update()
+        else:
+            l.backward()
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.optimizer.step()
+        return {'loss': l.detach().cpu().numpy()}
+
+    def validation_step(self, batch: dict) -> dict:
+        data = batch['data']
+        target = batch['target']
+
+        data = data.to(self.device, non_blocking=True)
+        if isinstance(target, list):
+            bbox_mask = target[0][:,-1:].to(self.device, non_blocking=True)
+            target = [i[:,:-1].to(self.device, non_blocking=True) for i in target]
+        else:
+            bbox_mask = target[:,-1:].to(self.device, non_blocking=True)
+            target = target[:,:-1].to(self.device, non_blocking=True)
+
+        data = torch.cat([data, bbox_mask], dim=1)
+
+        # Autocast can be annoying
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+            output = self.network(data)
+            del data
+            l = self.loss(output, target)
+
+        # we only need the output with the highest output resolution (if DS enabled)
+        if self.enable_deep_supervision:
+            output = output[0]
+            target = target[0]
+
+        # the following is needed for online evaluation. Fake dice (green line)
+        axes = [0] + list(range(2, output.ndim))
+
+        if self.label_manager.has_regions:
+            predicted_segmentation_onehot = (torch.sigmoid(output) > 0.5).long()
+        else:
+            # no need for softmax
+            output_seg = output.argmax(1)[:, None]
+            predicted_segmentation_onehot = torch.zeros(output.shape, device=output.device, dtype=torch.float32)
+            predicted_segmentation_onehot.scatter_(1, output_seg, 1)
+            del output_seg
+
+        if self.label_manager.has_ignore_label:
+            if not self.label_manager.has_regions:
+                mask = (target != self.label_manager.ignore_label).float()
+                # CAREFUL that you don't rely on target after this line!
+                target[target == self.label_manager.ignore_label] = 0
+            else:
+                mask = 1 - target[:, -1:]
+                # CAREFUL that you don't rely on target after this line!
+                target = target[:, :-1]
+        else:
+            mask = None
+
+        tp, fp, fn, _ = get_tp_fp_fn_tn(predicted_segmentation_onehot, target, axes=axes, mask=mask)
+
+        tp_hard = tp.detach().cpu().numpy()
+        fp_hard = fp.detach().cpu().numpy()
+        fn_hard = fn.detach().cpu().numpy()
+        if not self.label_manager.has_regions:
+            # if we train with regions all segmentation heads predict some kind of foreground. In conventional
+            # (softmax training) there needs tobe one output for the background. We are not interested in the
+            # background Dice
+            # [1:] in order to remove background
+            tp_hard = tp_hard[1:]
+            fp_hard = fp_hard[1:]
+            fn_hard = fn_hard[1:]
+
+        return {'loss': l.detach().cpu().numpy(), 'tp_hard': tp_hard, 'fp_hard': fp_hard, 'fn_hard': fn_hard}
