418f5201ef88d7fbefaf17681292578fbf1b1b1a
Yannick Kirchhoff <yannick.kirchhoff@dkfz-heidelberg.de>
2024-05-06 20:13:12 +0200

    add SAM dataloader and trainer

---
+++ nnunetv2/training/dataloading/data_loader_2d_sam.py
@@ -0,0 +1,97 @@
+from batchgenerators.utilities.file_and_folder_operations import List
+from skimage.morphology import dilation
+from skimage.measure import label
+from typing import Tuple
+import numpy as np
+from nnunetv2.training.dataloading.base_data_loader import nnUNetDataLoaderBase
+from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDataset
+from nnunetv2.utilities.label_handling.label_handling import LabelManager
+
+
+class nnUNetDataLoader2DSAM(nnUNetDataLoaderBase):
+    def __init__(self, data: nnUNetDataset, batch_size: int, patch_size: List[int] | Tuple[int] | np.ndarray, 
+                 final_patch_size: List[int] | Tuple[int] | np.ndarray, label_manager: LabelManager, 
+                 oversample_foreground_percent: float = 0, sampling_probabilities: List[int] | Tuple[int] | np.ndarray = None, 
+                 pad_sides: List[int] | Tuple[int] | np.ndarray = None, probabilistic_oversampling: bool = False,
+                 num_bbox: int = 1, bbox_dilation: int = 0):
+        super().__init__(data, batch_size, patch_size, final_patch_size, label_manager, oversample_foreground_percent, sampling_probabilities, pad_sides, probabilistic_oversampling)
+        self.num_bbox = num_bbox
+        self.bbox_dilation = bbox_dilation
+
+    def get_bbox(self, seg: np.ndarray, properties: dict):
+        target = np.random.choice(list(properties["mask_dict"].keys()))
+        labels = properties["mask_dict"][target]
+        bbox = properties["bboxs"][target-1]
+        bbox = list(map(int, [bbox[0][0], bbox[1][0], bbox[0][1], bbox[1][1]]))
+        seg_bin = np.where(np.isin(seg, list(labels)), 1, 0)
+        size = [bbox[1]-bbox[0], bbox[3]-bbox[2]]
+
+        need_to_pad = self.need_to_pad.copy()
+        data_shape = seg.shape[1:]
+        dim = len(data_shape)
+
+        for d in range(dim):
+            # if case_all_data.shape + need_to_pad is still < patch size we need to pad more! We pad on both sides
+            # always
+            if need_to_pad[d] + data_shape[d] < self.patch_size[d]:
+                need_to_pad[d] = self.patch_size[d] - data_shape[d]
+
+        # we can now choose the bbox from -need_to_pad // 2 to shape - patch_size + need_to_pad // 2. Here we
+        # define what the upper and lower bound can be to then sample form them with np.random.randint
+        bbox_lbs = [- need_to_pad[i] // 2 for i in range(dim)]
+        bbox_ubs = [bbox_lbs[i] + self.patch_size[i] for i in range(dim)]
+
+        # bbox_lbs = [np.random.randint(bbox[0]-max(0, self.patch_size[0]-size[0]), bbox[0]+1),
+                    # np.random.randint(bbox[2]-max(0, self.patch_size[1]-size[1]), bbox[2]+1)]
+        # bbox_ubs = [bbox_lbs[i] + self.patch_size[i] for i in range(2)]
+        bbox_mask = np.zeros_like(seg)
+        jitter = np.random.randint(0, self.bbox_dilation+1, size=4)
+        bbox_mask[:, bbox[0]-jitter[0]:bbox[1]+jitter[1], bbox[2]-jitter[2]:bbox[3]+jitter[3]] = 1
+
+        return bbox_lbs, bbox_ubs, np.concatenate([seg_bin.astype(seg.dtype), bbox_mask.astype(seg.dtype)], axis=0)
+
+    def generate_train_batch(self):
+        selected_keys = self.get_indices()
+        # preallocate memory for data and seg
+        data_all = np.zeros(self.data_shape, dtype=np.float32)
+        seg_all = np.zeros((self.seg_shape[0], 2, *self.seg_shape[2:]), dtype=np.int16)
+        case_properties = []
+
+        for j, current_key in enumerate(selected_keys):
+            # oversampling foreground will improve stability of model training, especially if many patches are empty
+            # (Lung for example)
+            data, seg, properties = self._data.load_case(current_key)
+            case_properties.append(properties)
+
+            selected_slice = np.random.choice(len(data[0]))
+
+            data = data[:, selected_slice]
+            seg = seg[:, selected_slice]
+
+            # print(properties)
+            shape = data.shape[1:]
+            dim = len(shape)
+            bbox_lbs, bbox_ubs, seg = self.get_bbox(seg, properties)
+
+            # whoever wrote this knew what he was doing (hint: it was me). We first crop the data to the region of the
+            # bbox that actually lies within the data. This will result in a smaller array which is then faster to pad.
+            # valid_bbox is just the coord that lied within the data cube. It will be padded to match the patch size
+            # later
+            valid_bbox_lbs = [max(0, bbox_lbs[i]) for i in range(dim)]
+            valid_bbox_ubs = [min(shape[i], bbox_ubs[i]) for i in range(dim)]
+
+            # At this point you might ask yourself why we would treat seg differently from seg_from_previous_stage.
+            # Why not just concatenate them here and forget about the if statements? Well that's because segneeds to
+            # be padded with -1 constant whereas seg_from_previous_stage needs to be padded with 0s (we could also
+            # remove label -1 in the data augmentation but this way it is less error prone)
+            this_slice = tuple([slice(0, data.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
+            data = data[this_slice]
+
+            this_slice = tuple([slice(0, seg.shape[0])] + [slice(i, j) for i, j in zip(valid_bbox_lbs, valid_bbox_ubs)])
+            seg = seg[this_slice]
+
+            padding = [(-min(0, bbox_lbs[i]), max(bbox_ubs[i] - shape[i], 0)) for i in range(dim)]
+            data_all[j] = np.pad(data, ((0, 0), *padding), 'constant', constant_values=0)
+            seg_all[j] = np.pad(seg, ((0, 0), *padding), 'constant', constant_values=-1)
+
+        return {'data': data_all, 'seg': seg_all, 'properties': case_properties, 'keys': selected_keys}
---
+++ nnunetv2/training/nnUNetTrainer/variants/coding_power_unit/nnUNetTrainerSAM.py
@@ -0,0 +1,199 @@
+from typing import Union, Tuple, List
+
+import torch
+from batchgenerators.transforms.abstract_transforms import AbstractTransform, Compose
+from batchgenerators.transforms.utility_transforms import RemoveLabelTransform, RenameTransform, NumpyToTensor
+
+from nnunetv2.training.data_augmentation.custom_transforms.cascade_transforms import MoveSegAsOneHotToData
+from nnunetv2.training.data_augmentation.custom_transforms.deep_supervision_donwsampling import \
+    DownsampleSegForDSTransform2
+from nnunetv2.training.data_augmentation.custom_transforms.region_based_training import \
+    ConvertSegmentationToRegionsTransform
+from nnunetv2.training.loss.dice import get_tp_fp_fn_tn
+from nnunetv2.utilities.get_network_from_plans import get_network_from_plans
+from nnunetv2.utilities.helpers import dummy_context
+from torch import autocast, nn
+
+from nnunetv2.training.dataloading.data_loader_2d_sam import nnUNetDataLoader2DSAM
+
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+
+
+class nnUNetTrainerSAM(nnUNetTrainer):
+    @staticmethod
+    def build_network_architecture(architecture_class_name: str,
+                                   arch_init_kwargs: dict,
+                                   arch_init_kwargs_req_import: Union[List[str], Tuple[str, ...]],
+                                   num_input_channels: int,
+                                   num_output_channels: int,
+                                   enable_deep_supervision: bool = True) -> nn.Module:
+        return get_network_from_plans(
+            architecture_class_name,
+            arch_init_kwargs,
+            arch_init_kwargs_req_import,
+            num_input_channels+1,
+            num_output_channels,
+            allow_init=True,
+            deep_supervision=enable_deep_supervision)
+
+    @staticmethod
+    def get_validation_transforms(
+            deep_supervision_scales: Union[List, Tuple, None],
+            is_cascaded: bool = False,
+            foreground_labels: Union[Tuple[int, ...], List[int]] = None,
+            regions: List[Union[List[int], Tuple[int, ...], int]] = None,
+            ignore_label: int = None,
+    ) -> AbstractTransform:
+        val_transforms = []
+        val_transforms.append(RemoveLabelTransform(-1, 0))
+
+        if is_cascaded:
+            val_transforms.append(MoveSegAsOneHotToData(1, foreground_labels, 'seg', 'data'))
+
+        val_transforms.append(RenameTransform('seg', 'target', True))
+
+        if regions is not None:
+            # the ignore label must also be converted
+            val_transforms.append(ConvertSegmentationToRegionsTransform(list(regions) + [ignore_label]
+                                                                        if ignore_label is not None else regions,
+                                                                        'target', 'target'))
+
+        if deep_supervision_scales is not None:
+            val_transforms.append(DownsampleSegForDSTransform2(deep_supervision_scales, 0, input_key='target',
+                                                               output_key='target'))
+
+        val_transforms.append(NumpyToTensor(['data', 'target'], 'float'))
+        val_transforms = Compose(val_transforms)
+        return val_transforms
+
+    def get_plain_dataloaders(self, initial_patch_size: Tuple[int, ...], dim: int):
+        dataset_tr, dataset_val = self.get_tr_and_val_datasets()
+
+        if dim == 2:
+            dl_tr = nnUNetDataLoader2DSAM(dataset_tr, self.batch_size,
+                                       initial_patch_size,
+                                       self.configuration_manager.patch_size,
+                                       self.label_manager,
+                                       oversample_foreground_percent=self.oversample_foreground_percent,
+                                       sampling_probabilities=None, pad_sides=None,
+                                       num_bbox=1, bbox_dilation=20)
+            dl_val = nnUNetDataLoader2DSAM(dataset_val, self.batch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.label_manager,
+                                        oversample_foreground_percent=self.oversample_foreground_percent,
+                                        sampling_probabilities=None, pad_sides=None,
+                                        num_bbox=1, bbox_dilation=20)
+        else:
+            raise NotImplementedError("3D dataloader not implemented")
+        return dl_tr, dl_val
+
+    def modify_output(self, output, bbox_mask):
+        return output
+
+    def train_step(self, batch: dict) -> dict:
+        data = batch['data']
+        target = batch['target']
+
+        data = data.to(self.device, non_blocking=True)
+        if isinstance(target, list):
+            bbox_mask = target[0][:,-1:].to(self.device, non_blocking=True)
+            target = [i[:,:-1].to(self.device, non_blocking=True) for i in target]
+        else:
+            bbox_mask = target[:,-1:].to(self.device, non_blocking=True)
+            target = target[:,:-1].to(self.device, non_blocking=True)
+
+        data = torch.cat([data, bbox_mask], dim=1)
+
+        self.optimizer.zero_grad(set_to_none=True)
+        # Autocast can be annoying
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+            output = self.network(data)
+            # del data
+
+            output = self.modify_output(output, bbox_mask)
+            l = self.loss(output, target)
+
+        if self.grad_scaler is not None:
+            self.grad_scaler.scale(l).backward()
+            self.grad_scaler.unscale_(self.optimizer)
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.grad_scaler.step(self.optimizer)
+            self.grad_scaler.update()
+        else:
+            l.backward()
+            torch.nn.utils.clip_grad_norm_(self.network.parameters(), 12)
+            self.optimizer.step()
+        return {'loss': l.detach().cpu().numpy()}
+
+    def validation_step(self, batch: dict) -> dict:
+        data = batch['data']
+        target = batch['target']
+
+        data = data.to(self.device, non_blocking=True)
+        if isinstance(target, list):
+            bbox_mask = target[0][:,-1:].to(self.device, non_blocking=True)
+            target = [i[:,:-1].to(self.device, non_blocking=True) for i in target]
+        else:
+            bbox_mask = target[:,-1:].to(self.device, non_blocking=True)
+            target = target[:,:-1].to(self.device, non_blocking=True)
+
+        data = torch.cat([data, bbox_mask], dim=1)
+
+        # Autocast can be annoying
+        # If the device_type is 'cpu' then it's slow as heck and needs to be disabled.
+        # If the device_type is 'mps' then it will complain that mps is not implemented, even if enabled=False is set. Whyyyyyyy. (this is why we don't make use of enabled=False)
+        # So autocast will only be active if we have a cuda device.
+        with autocast(self.device.type, enabled=True) if self.device.type == 'cuda' else dummy_context():
+            output = self.network(data)
+            del data
+            output = self.modify_output(output, bbox_mask)
+            l = self.loss(output, target)
+
+        # we only need the output with the highest output resolution (if DS enabled)
+        if self.enable_deep_supervision:
+            output = output[0]
+            target = target[0]
+
+        # the following is needed for online evaluation. Fake dice (green line)
+        axes = [0] + list(range(2, output.ndim))
+
+        if self.label_manager.has_regions:
+            predicted_segmentation_onehot = (torch.sigmoid(output) > 0.5).long()
+        else:
+            # no need for softmax
+            output_seg = output.argmax(1)[:, None]
+            predicted_segmentation_onehot = torch.zeros(output.shape, device=output.device, dtype=torch.float32)
+            predicted_segmentation_onehot.scatter_(1, output_seg, 1)
+            del output_seg
+
+        if self.label_manager.has_ignore_label:
+            if not self.label_manager.has_regions:
+                mask = (target != self.label_manager.ignore_label).float()
+                # CAREFUL that you don't rely on target after this line!
+                target[target == self.label_manager.ignore_label] = 0
+            else:
+                mask = 1 - target[:, -1:]
+                # CAREFUL that you don't rely on target after this line!
+                target = target[:, :-1]
+        else:
+            mask = None
+
+        tp, fp, fn, _ = get_tp_fp_fn_tn(predicted_segmentation_onehot, target, axes=axes, mask=mask)
+
+        tp_hard = tp.detach().cpu().numpy()
+        fp_hard = fp.detach().cpu().numpy()
+        fn_hard = fn.detach().cpu().numpy()
+        if not self.label_manager.has_regions:
+            # if we train with regions all segmentation heads predict some kind of foreground. In conventional
+            # (softmax training) there needs tobe one output for the background. We are not interested in the
+            # background Dice
+            # [1:] in order to remove background
+            tp_hard = tp_hard[1:]
+            fp_hard = fp_hard[1:]
+            fn_hard = fn_hard[1:]
+
+        return {'loss': l.detach().cpu().numpy(), 'tp_hard': tp_hard, 'fp_hard': fp_hard, 'fn_hard': fn_hard}
